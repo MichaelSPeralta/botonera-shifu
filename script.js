fa94bf568a7e135212366833a4b533c252dfebb4
MichaelSPeralta <53713516+MichaelSPeralta@users.noreply.github.com>
2020-08-27 19:40:01 -0300

    Estilos funcionales a los botones

--- script.js
+++ script.js
@@ -1,7 +1,79 @@
-let changebtn = document.getElementById("vestidor");
+function fComedor() {
+  let comedor = document.getElementById("comedor");
 
-changebtn.addEventListener("click", () => {
-    document.getElementById("vestidor").setAttribute("class", "btn-active");
-});
+  if (comedor.className == "btn") {
+    document.getElementById("comedor").className = "btn-active";
+  } else {
+    document.getElementById("comedor").className = "btn";
+  }
+}
 
+function fBano() {
+  let bano = document.getElementById("bano");
 
+  if (bano.className == "btn") {
+    document.getElementById("bano").className = "btn-active";
+  } else {
+    document.getElementById("bano").className = "btn";
+  }
+}
+
+function fSala() {
+  let sala = document.getElementById("sala");
+
+  if (sala.className == "btn") {
+    document.getElementById("sala").className = "btn-active";
+  } else {
+    document.getElementById("sala").className = "btn";
+  }
+}
+
+function fCocina() {
+  let cocina = document.getElementById("cocina");
+
+  if (cocina.className == "btn") {
+    document.getElementById("cocina").className = "btn-active";
+  } else {
+    document.getElementById("cocina").className = "btn";
+  }
+}
+
+function fHabitacion() {
+  let habitacion = document.getElementById("habitacion");
+
+  if (habitacion.className == "btn") {
+    document.getElementById("habitacion").className = "btn-active";
+  } else {
+    document.getElementById("habitacion").className = "btn";
+  }
+}
+
+function fOficina() {
+  let oficina = document.getElementById("oficina");
+
+  if (oficina.className == "btn") {
+    document.getElementById("oficina").className = "btn-active";
+  } else {
+    document.getElementById("oficina").className = "btn";
+  }
+}
+
+function fVani() {
+  let vani = document.getElementById("vani");
+
+  if (vani.className == "btn") {
+    document.getElementById("vani").className = "btn-active";
+  } else {
+    document.getElementById("vani").className = "btn";
+  }
+}
+
+function fVestidor() {
+  let vestidor = document.getElementById("vestidor");
+
+  if (vestidor.className == "btn") {
+    document.getElementById("vestidor").className = "btn-active";
+  } else {
+    document.getElementById("vestidor").className = "btn";
+  }
+}
