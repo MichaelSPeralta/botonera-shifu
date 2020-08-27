a580a860efc5c585e852f018823d01cb9f8062e4
MichaelSPeralta <michael69.piro@gmail.com>
2020-08-27 14:11:46 -0300

    change button styles

---
+++ script.js
@@ -0,0 +1,7 @@
+let changebtn = document.getElementById("vestidor");
+
+changebtn.addEventListener("click", () => {
+    document.getElementById("vestidor").setAttribute("class", "btn-active");
+});
+
+
